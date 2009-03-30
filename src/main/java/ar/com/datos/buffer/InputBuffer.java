102bd7b27df4458f3420c5270e38f518dbc2ca3c
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-03-30 03:41:58 +0000

    Agrego documentacion a la interfaz InputBuffer

--- src/main/java/ar/com/datos/buffer/InputBuffer.java
+++ src/main/java/ar/com/datos/buffer/InputBuffer.java
@@ -3,12 +3,24 @@ package ar.com.datos.buffer;
 import ar.com.datos.buffer.exception.BufferException;
 
 /**
- * Interfacez creada para hacer algunas pruebas
- * debería ser reemplazada por la de Marcos
- * @author Juan Manuel Barreneche
- *
+ * Se encarga de manejar un buffer interno para operaciones de lectura.
+ * Utiliza un array interno como buffer para los bytes que se van leyendo
  */
 public interface InputBuffer {
+	/**
+	 * Lee una cantidad n de datos del buffer, siendo n
+     * el size del array de data.
+	 * @param data un array con el size que se quiera leer en el buffer
+	 * @return un array de bytes leidos del buffer
+	 * @throws BufferException si no hay suficientes datos para leer desde el buffer
+	 */
 	public byte[] read(byte[] data) throws BufferException;
+	
+	/**
+	 * Lee el siguiente byte de datos.
+	 * 
+	 * @return el siguiente byte leido desde el buffer.
+	 * @throws BufferException si no hay suficientes datos para leer desde el buffer
+	 */
 	public byte read() throws BufferException;
 }
