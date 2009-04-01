9ecb773e6f8944aebefe85f7fa9a637e0e987f76
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-04-01 15:56:27 +0000

    - Avance de Main, me falta testear el funcionamiento.

--- src/main/java/ar/com/datos/Main.java
+++ src/main/java/ar/com/datos/Main.java
@@ -1,53 +1,161 @@
 package ar.com.datos;
 
-import ar.com.datos.audio.AudioServiceHandler;
-import ar.com.datos.audio.exception.AudioServiceHandlerException;
-
-import ar.com.datos.wordandsoundservice.WaSService;
-import ar.com.datos.wordandsoundservice.variableLength.WaSSVariableLengthImpl;
+import ar.com.datos.audio.IConectorUsuarioGrabadorPalabras;
+import ar.com.datos.audio.ReproductorPalabras;
+import ar.com.datos.audio.GrabadorPalabras;
 
 import ar.com.datos.parser.IParser;
 import ar.com.datos.parser.SimpleTextParser;
+import java.io.*;
+import java.util.Collection;
 
 /**
- *
  *
  * @author mfernandez
  *
  */
 
-public class Main {
+public class Main implements IConectorUsuarioGrabadorPalabras{
 
-	private AudioServiceHandler servicioAudio;
-	private WaSService servicioArchivos;
-	private IParser parser;
+    private IParser parser;
+    private BufferedReader bufferReaderTeclado;
+    private ReproductorPalabras reproductor;
+    private GrabadorPalabras grabador;
+
+    public Main() {
+
+        parser				= new SimpleTextParser();
+        bufferReaderTeclado = new BufferedReader(new InputStreamReader(System.in));
+        reproductor 		= new ReproductorPalabras();
+        grabador			= new GrabadorPalabras(this);
+
+    }
+
+    public void init() {
+        showMenu();
+    }
 
 
-	public Main() {
+    @Override
+    public void notificarPalabra(String palabra){
+        System.out.println("Se ha encontrado la palabra " + palabra);
+    }
 
-		servicioAudio 	 = new AudioServiceHandler();
-		servicioArchivos = new WaSSVariableLengthImpl();
-		parser			 = new SimpleTextParser();
+    @Override
+    public boolean iniciarGrabacion(){
+        String opcion = leerStringTeclado();
+        return opcion == "i";
+    }
+
+    @Override
+    public boolean palabraGrabadaCorrectamente(){
+        System.out.println("Opciones:");
+        System.out.println("s: Guardar la palabra.");
+        System.out.println("Grabar nuevamente (cualquier otra tecla).");
+        String opcion = leerStringTeclado();
+        return opcion == "s";
+    }
+
+    @Override
+    public void notificarErrorGrabacion(){
+
+    }
+
+    public void grabacionIniciada(){
+        String opcion = leerStringTeclado();
+
+        if (opcion == "f"){
+            grabador.stopRecording();
+        }
+        else {
+            grabacionIniciada();
+        }
+    }
 
 
-	}
+    /**
+     * @return String leido por teclado
+     */
+    private String leerStringTeclado(){
+        String linea = "";
+        try{
+            linea = bufferReaderTeclado.readLine();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return linea;
+    }
 
-	public void init() {
-		showMenu();
-	}
+    /**
+     * Menu Inicial
+     *
+     */
+    private void showMenu() {
+        System.out.println("Opciones:");
+        System.out.println("1 - Carga de documentos");
+        System.out.println("2 - Reproducción de palabras");
+        System.out.println("Seleccione una opcion:");
 
-	private void showMenu() {
+        String tecla = leerStringTeclado();
 
-	}
+        switch(tecla.charAt(0)){
+        case '1': cargarDocumento();break;
+        case '2': reproducirDocumento();break;
+        }
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		Main app = new Main();
-		app.init();
-	}
+    /**
+     * Solicita al usuario la ruta del documento e intenta parsearlo,
+     * y luego guarda las palabras no existentes
+     */
+    private void cargarDocumento(){
+
+        System.out.println("Ingrese una ruta valida:");
+
+        String ruta = leerStringTeclado();
+        Collection<String> palabras = null;
+
+        try {
+            palabras = parser.parseTextFile(ruta);
+            grabador.guardarPalabras(palabras);
+        }
+        catch(Exception e){
+            cargarDocumento();
+        }
+
+    }
+
+    /**
+     * Solicita al usuairo la ruta del documento, intenta parsearlo y reproduce
+     * cada una de las palabras.
+     *
+     */
+    private void reproducirDocumento(){
+        System.out.println("Ingrese una ruta valida:");
+
+        String ruta = leerStringTeclado();
+        Collection<String> palabras = null;
+
+        try {
+            palabras = parser.parseTextFile(ruta);
+            reproductor.reproducirPalabras(palabras);
+        }
+        catch(Exception e){
+            reproducirDocumento();
+        }
+    }
+
+    /**
+     * Main
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Main app = new Main();
+        app.init();
+    }
 
 }
---
+++ src/main/java/ar/com/datos/audio/ReproductorPalabras.java
@@ -0,0 +1,70 @@
+package ar.com.datos.audio;
+
+import java.io.InputStream;
+import java.util.Collection;
+
+import ar.com.datos.persistencia.SoundPersistenceService;
+import ar.com.datos.persistencia.exception.UnregisteredWordException;
+import ar.com.datos.persistencia.variableLength.SoundPersistenceServiceVariableLengthImpl;
+
+/**
+ * Reproductor de palabras
+ *
+ * @author mfernandez
+ *
+ */
+public class ReproductorPalabras {
+
+     private AudioServiceHandler servicioAudio;
+     private SoundPersistenceService servicioArchivos;
+     private boolean reproduciendo;
+
+     public ReproductorPalabras(){
+         servicioAudio = AudioServiceHandler.getInstance();
+         servicioArchivos = new SoundPersistenceServiceVariableLengthImpl();
+         reproduciendo = false;
+     }
+
+     /**
+     * Reproduce cada una de las palabras de la coleccion
+     * @param palabras
+     */
+    public void reproducirPalabras(Collection<String> palabras){
+
+        if (!reproduciendo){
+
+            reproduciendo = true;
+
+            for (String palabra : palabras){
+
+                try{
+                    InputStream audio = servicioArchivos.readWord(palabra);
+
+                    /** No puedo reproducir directamente, deberia esperar que la reproducccion
+                    * anterior termine. Para ello obtengo el Thread de reproduccion y llamo al metodo join()
+                    *
+                    * @see http://java.sun.com/j2se/1.5.0/docs/api/java/lang/Thread.html#join()
+                    *
+                    */
+                    Thread reproduccion = servicioAudio.play(audio);
+
+                    try {
+                        //Espero que termine la reproduccion
+                        reproduccion.join();
+                    }
+                    catch(InterruptedException e){
+                        System.out.println("Thread principal interrumpido");
+                    }
+
+                    //Desbloqueo el servicio de audio
+                    servicioAudio.stopPlaying();
+
+                }
+                catch (UnregisteredWordException e){
+                    // Audio no encontrado, beep??
+                }
+
+            }
+        }
+    }
+}
