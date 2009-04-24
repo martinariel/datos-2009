633695180411f499d525727659985dc6c2f1b841
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-04-24 21:58:10 +0000

    aa

--- src/main/java/ar/com/datos/file/DynamicAccesor.java
+++ src/main/java/ar/com/datos/file/DynamicAccesor.java
@@ -1,9 +1,12 @@
 package ar.com.datos.file;
 
+
 public interface DynamicAccesor<T> extends SequentialAccesor<T> {
 
 	public T get(Address<Long, Short> direccion);
 
 	public Address<Long, Short> updateEntity(Address<Long, Short> direccion, T object);
 
+//	public Short getAmountOfBlocksFor(Address<Long, Short> direccion);
+//	public Integer getDataSizeFor(Short numberOfChainedBlocks);
 }
