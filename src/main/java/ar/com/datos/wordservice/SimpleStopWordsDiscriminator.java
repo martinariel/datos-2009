bdd1d5f200d064633bb02da045473ff0b57d5689
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-04-24 16:15:33 +0000

    Primera version del algoritmo de discriminacion de stopwords y stopphrases. Parece funcionar bien, ahora escribo los tests.

--- src/main/java/ar/com/datos/wordservice/SimpleStopWordsDiscriminator.java
+++ src/main/java/ar/com/datos/wordservice/SimpleStopWordsDiscriminator.java
@@ -1,56 +1,64 @@
 package ar.com.datos.wordservice;
 
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
-import ar.com.datos.wordservice.exception.ActiveSessionException;
-
+@SuppressWarnings("unchecked")
 public class SimpleStopWordsDiscriminator implements StopWordsDiscriminator {
-
-	private boolean sessionActive;
-	private Set<String> stopwords;
-	private Set<String> sessionStopWords;
-	private Set<String> sessionNonStopWords;
 	
-	public SimpleStopWordsDiscriminator(Set stopwords){
-		this.stopwords = stopwords;
+	private Hashtable stopPhrases;
+	private Set<String> stopWords;
+	
+	public SimpleStopWordsDiscriminator(Set<String> stopWords, Hashtable stopPhrases){
+		this.stopWords = stopWords;
+		this.stopPhrases = stopPhrases;
 	}
 	
-	@Override
-	public Set<String> getNonStopWords() {
-		if (this.isActive()) throw new ActiveSessionException("Cannot get session NonStopWords, the session is still active.");
-		return this.sessionNonStopWords;
-	}
-
-	@Override
-	public Set<String> getStopWords() {
-		if (this.isActive()) throw new ActiveSessionException("Cannot get session StopWords, the session is still active.");
-		return this.sessionStopWords;
-	}
-
 	private boolean isStopWord(String word){
-		return this.stopwords.contains(word);
+		return this.stopWords.contains(word);
 	}
 	
-	@Override
-	public void processWord(String word) {
-		if (this.isStopWord(word)){ 
-			this.sessionStopWords.add(word);
-		} else {
-			this.sessionNonStopWords.add(word);
+	@ Override
+	public List<String> processPhrase(List<String> phrase) {
+		String word;
+		Hashtable hash = this.stopPhrases;
+		List<String> tempWords = new LinkedList<String>();
+		List<String> nonStopWords = new LinkedList<String>();
+		
+		// a phrase must contain at least 2 words.
+		if (phrase.size()==0) return nonStopWords;
+		if (phrase.size()==1){
+			word = phrase.get(0);
+			if (this.isStopWord(word)){
+				nonStopWords.add(word);
+			}
+			return nonStopWords;
 		}
+		// we have at least 2 words in phrase
+		
+		int i = 0;
+		while(!phrase.isEmpty()){
+			word = phrase.get(i);
+			if (hash.containsKey(word)){
+				tempWords.add(word);
+				hash = (Hashtable) hash.get(word);
+				if (hash == null){ 			// stop phrase found!
+					tempWords.clear();
+					for (int j=0; j<i; j++) // remove stop phrase from 'phrase'
+						phrase.remove(j);	// we only need NON-stop words/phrases
+					i=0;
+				}
+				i++;
+			} else {
+				if (this.isStopWord(word)){
+					phrase.remove(i);		// is stop word, forget it
+				} else {
+					nonStopWords.add(phrase.remove(i));	// non stop word, keep it!
+				}
+			}
+		}
+		return nonStopWords; 
 	}
-
-	@Override
-	public void endSession() {
-		this.sessionActive = false;
-	}
-	@Override
-	public void startSession() {
-		this.sessionActive = true;
-	}
-	@Override
-	public boolean isActive() {
-		return this.sessionActive;
-	}
-
 }
