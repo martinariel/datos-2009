f7385a7baa736266b9ef5eab3aa0ad806224957e
marcosmedrano0 <marcosmedrano0@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-24 16:12:14 +0000

    Version final(?) de la interfaz del discriminator

--- trunk/src/main/java/ar/com/datos/wordservice/StopWordsDiscriminator.java
+++ trunk/src/main/java/ar/com/datos/wordservice/StopWordsDiscriminator.java
@@ -1,8 +1,6 @@
 package ar.com.datos.wordservice;
 
-import java.util.Set;
-
-import ar.com.datos.wordservice.exception.ActiveSessionException;
+import java.util.List;
 
 /**
  * Un StopWordsDiscriminator se encarga de discriminar que palabras son o no
@@ -12,37 +10,22 @@ import ar.com.datos.wordservice.exception.ActiveSessionException;
  * suelen no ser irrelevantes para el mismo. Estas palabras no deben indexarse
  * con lo cual es necesario discriminarlas.
  * 
- * Las implementaciones de StopWordsDiscriminator funcionaran mediante sesiones.
- * Para cada sesion se almacenara las palabras stopwords y las nonstopwords de 
- * manera que el usuario de dicha implementacion pueda consultarlas al finalizar
- * la sesion.
+ * También hay palabras que comúnmente aparecen en locuciones, como en el caso 
+ * de algunos conectores de coordinación (p.e. "en consecuencia", "de cualquier 
+ * manera", "en caso de que"), que en ese contexto no tienen significado 
+ * relevante aunque individualmente fuera de ese contexto sí, por lo que debe 
+ * considerarse la detección no sólo de palabras individuales (stop words) sino 
+ * de secuencias de palabras (que llamaremos stop phrases).
  *  
  * @author Marcos J. Medrano
  */
-public interface StopWordsDiscriminator extends SessionHandler {
+public interface StopWordsDiscriminator{
 	
 	/**
-	 * Procesa una palabra para ver si es o no una stopword. El resultado de
-	 * cada procesamiento deberia ser almacenado en dos {@link Set}s. Uno para
-	 * stopwords y otro para nonstopwords. 
-	 * @param word la palabra a analizar
-	 * @throws {@link ActiveSessionException} si la session no esta activa
+	 * Procesa una frase identificando stop words o stop phrases.
+	 * @param phrase una List<String> con la frase a analizar
+	 * @return una List<String> de las palabras que NO son stop words ni stop phrases.
 	 */
-	public void processWord(String word);
-	
-	/**
-	 * Devuelve en un {@link Set<String>} las stopwords encontradas durante la 
-	 * sesion. La sesion debe estar cerrada o se lanzara una excepcion.  
-	 * @throws {@link ActiveSessionException} si la sesion esta activa
-	 * @return {@link Set<String>} las stopwords procesadas en la session
-	 */
-	public Set<String> getStopWords();
-	
-	/**
-	 * Devuelve en un {@link Set<String>} las stopwords encontradas durante la 
-	 * sesion. La sesion debe estar cerrada o se lanzara una excepcion.  
-	 * @throws {@link ActiveSessionException} si la session esta activa
-	 * @return {@link Set<String>} las palabras que no son stopwords de la session
-	 */
-	public Set<String> getNonStopWords();
+	public List<String> processPhrase(List<String> phrase);
+
 }
