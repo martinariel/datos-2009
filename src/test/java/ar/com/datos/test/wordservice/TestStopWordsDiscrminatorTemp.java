803e4bbfa8072e6e88d2085deb99448f36717829
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-04-26 04:59:29 +0000

    Finalmente logro que funcione el StopWord discriminator (Agrego tuplas para marcar el fin de una stop phrase). Agrego test cases.

---
+++ src/test/java/ar/com/datos/test/wordservice/TestStopWordsDiscrminatorTemp.java
@@ -0,0 +1,95 @@
+package ar.com.datos.test.wordservice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import ar.com.datos.util.Tuple;
+import ar.com.datos.wordservice.stopwords.SimpleStopWordsDiscriminator;
+import ar.com.datos.wordservice.stopwords.StopWordsDiscriminator;
+import junit.framework.TestCase;
+
+public class TestStopWordsDiscrminatorTemp extends TestCase {
+	
+	private List<List<String>> stopPhrases;
+	
+	public void setUp(){
+		stopPhrases = new LinkedList<List<String>>();
+		
+		String stopPhrase1 = "en consecuencia";
+		String stopPhrase2 = "en consecuencia de";
+		String stopPhrase3 = "cualquier otra";
+		String stopPhrase6 = "en todas sus formas";
+		String stopPhrase7 = "en todas las formas";
+		stopPhrases.add(Arrays.asList(stopPhrase1.split(" ")));
+		stopPhrases.add(Arrays.asList(stopPhrase2.split(" ")));
+		stopPhrases.add(Arrays.asList(stopPhrase3.split(" ")));
+		stopPhrases.add(Arrays.asList(stopPhrase6.split(" ")));
+		stopPhrases.add(Arrays.asList(stopPhrase7.split(" ")));
+	}
+	@SuppressWarnings("unchecked")
+	public void testHashConstruction(){
+		Hashtable h = this.constructStopPhrasesHashChain(stopPhrases);
+		
+		assertTrue(h.containsKey("en"));
+		assertTrue(h.containsKey("cualquier"));
+		
+		Tuple<Boolean, Hashtable> t = (Tuple<Boolean, Hashtable>) h.get("cualquier");
+		assertFalse(t.getFirst());
+		assertTrue(t.getSecond().containsKey("otra"));
+		
+		t = (Tuple<Boolean, Hashtable>) t.getSecond().get("otra");
+		assertTrue(t.getFirst());
+		
+		t = (Tuple<Boolean, Hashtable>) h.get("en");
+		assertFalse(t.getFirst());
+		assertTrue(t.getSecond().containsKey("consecuencia"));
+		assertTrue(t.getSecond().containsKey("todas"));
+		
+		t = (Tuple<Boolean, Hashtable>) t.getSecond().get("consecuencia");
+		assertTrue(t.getSecond().containsKey("de"));
+		assertTrue(t.getFirst());
+		
+		t = (Tuple<Boolean, Hashtable>) t.getSecond().get("de");
+		assertTrue(t.getFirst());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Hashtable constructStopPhrasesHashChain(List<List<String>> phrases){
+		Hashtable h;
+		List<String> phrase;
+		Hashtable sessionStopPhrases = new Hashtable();
+		Tuple<Boolean, Hashtable> t;
+		
+		for (int i=0; i < phrases.size(); i++){
+			phrase = phrases.get(i);
+			h = sessionStopPhrases;
+			for (int j=0; j < phrase.size(); j++){
+				if (h.containsKey(phrase.get(j))){
+					h = ((Tuple<Boolean, Hashtable>) h.get(phrase.get(j))).getSecond();
+				} else {
+					h.put(phrase.get(j),
+    					this.createHashChainElement(phrase.subList(j+1, phrase.size())));
+					break;
+				}
+			}
+		}
+		return sessionStopPhrases;
+	}
+	@SuppressWarnings("unchecked")
+	private Tuple<Boolean, Hashtable> createHashChainElement(List<String> subphrase){
+		Hashtable h = new Hashtable();
+		boolean isPhraseEnd = subphrase.size() < 1;
+		if (!isPhraseEnd){
+			h.put(subphrase.get(0),
+			    this.createHashChainElement(subphrase.subList(1, subphrase.size())));	
+		}
+		return new Tuple<Boolean, Hashtable>(isPhraseEnd,h);
+	}
+	
+}
