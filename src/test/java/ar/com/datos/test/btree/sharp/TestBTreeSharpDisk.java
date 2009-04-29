92838c4640580e69811bc191de038deb067caa32
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-04-29 20:06:06 +0000

    Vuelvo a commitear para que Juan pueda ver el bug del vlfm...

--- src/test/java/ar/com/datos/test/btree/sharp/TestBTreeSharpDisk.java
+++ src/test/java/ar/com/datos/test/btree/sharp/TestBTreeSharpDisk.java
@@ -30,7 +30,7 @@ public class TestBTreeSharpDisk extends ExtendedTestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 
-		this.bTreeSharp = new BTreeSharpFactory<TestElementDisk, TestKeyDisk>().createBTreeSharpDisk(INTERNAL_FILE, LEAF_FILE, 128, 128, TestElementAndTestKeyListSerializerFactory.class, true);
+		this.bTreeSharp = new BTreeSharpFactory<TestElementDisk, TestKeyDisk>().createBTreeSharpDisk(INTERNAL_FILE, LEAF_FILE, 256, 256, TestElementAndTestKeyListSerializerFactory.class, true);
 	}
 
 	@Override
@@ -49,9 +49,11 @@ public class TestBTreeSharpDisk extends ExtendedTestCase {
 		try {
 			BTree<TestElementDisk, TestKeyDisk> btree = this.bTreeSharp;
 //			String[] lista = new String[] {"522630", "925861336572", "940027", "12275214072481695", "676159818757214", "635144937520", "343025", "885287", "855731162813196136"};
-			String[] lista = new String[] {"990707", "814561", "893024", "95592444739", "655170748521613866", "57971836335", "7775729897", "669833", "864004652398", "563959408814957898", "286823968160", "793501205650896534", "164502591619", "451579837255", "911722453988197619", "978636376735", "932372780949243559", "201413492933922261",  "316785921097439538", "119584708352261883", "365493879233", "869601452491", "755771699631354201", "536773693357", "545499"};
+//			String[] lista = new String[] {"990707", "814561", "893024", "95592444739", "655170748521613866", "57971836335", "7775729897", "669833", "864004652398", "563959408814957898", "286823968160", "793501205650896534", "164502591619", "451579837255", "911722453988197619", "978636376735", "932372780949243559", "201413492933922261",  "316785921097439538", "119584708352261883", "365493879233", "869601452491", "755771699631354201", "536773693357", "545499"};
+//			String[] lista = new String[] {"681522", "605085302661", "819896552595", "713905", "83106610053", "304329", "370921", "653743998796184703", "507495", "524550850161", "754145623790", "863771131624", "391649315367", "51417680752", "597275", "274884162412328600", "741142258335375664", "425729761484", "41812602287256919", "599626"};
+			String[] lista = new String[] {"843852175524142970", "320287", "89816", "233434533092", "251759213173", "606654", "802892", "69112", "647556794147", "880056307831", "916873820676", "643302883478861439", "641183542966", "858160174512", "388741162467", "256422877950", "902223432503", "679611", "802362538754", "745661464368", "794068688209", "757732912895734091", "944056156303", "379503"};
 			for (int i = 0; i < lista.length; i++) {
-				if (i == 13) {
+				if (i == 18) {
 					nada();
 				}
 				btree.addElement(new TestElementDisk(lista[i], 1));
@@ -61,7 +63,6 @@ public class TestBTreeSharpDisk extends ExtendedTestCase {
 			TestKeyDisk key;
 			for (int i = 0; i < lista.length; i++) {
 				key = new TestKeyDisk(lista[i]);
-				System.out.println(key);
 				element = btree.findElement(key);
 				assertNotNull(element);
 				assertEquals(key, element.getKey());
@@ -89,7 +90,7 @@ public class TestBTreeSharpDisk extends ExtendedTestCase {
 //			Integer k = null;
 //			String keyValue;
 //			List<String> inserted = new ArrayList<String>();
-//			for(int i = 0; i < 10000; i++){
+//			for(int i = 0; i < 25; i++){
 //				// Formo la clave como la concatenación de entre 1 y 3 números (así hago que
 //				// la clave mida diferentes tamaños).
 //				randomCount = (int) Math.round((Math.random()*2)) + 1;
