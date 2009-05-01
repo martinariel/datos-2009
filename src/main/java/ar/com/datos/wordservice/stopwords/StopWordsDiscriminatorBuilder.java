a6f0cccf21dcf99a13e2ef561cff56b2ec792909
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-05-01 22:13:51 +0000

    - Builder de StopWordsDiscriminator  - SimpleCrawler devolviendo todos las palabras del doc.

---
+++ src/main/java/ar/com/datos/wordservice/stopwords/StopWordsDiscriminatorBuilder.java
@@ -0,0 +1,54 @@
+package ar.com.datos.wordservice.stopwords;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import ar.com.datos.documentlibrary.Document;
+import ar.com.datos.documentlibrary.FileSystemDocument;
+
+/**
+ * Builder de StopWordsDiscriminator
+ * 
+ * @author martin
+ *
+ */
+public class StopWordsDiscriminatorBuilder {
+	
+	/**
+	 * Devuelve un StopWordsDiscriminator cargado con stopwords y stopphrases
+	 * buscando en los archivos stop_words.txt y stop_phrases.txt del directorio
+	 * especificado.
+	 * 
+	 * @param fileDirectory
+	 * @return
+	 */
+	public static StopWordsDiscriminator buildStopWords(String fileDirectory){
+    	
+    	Set<String> stopWords 			= new HashSet<String>();
+    	List<List<String>> stopPhrases 	= new LinkedList<List<String>>();
+    	Document stopWordsDocument		= new FileSystemDocument(fileDirectory + "stop_words.txt");
+    	Document stopPhrasesDocument	= new FileSystemDocument(fileDirectory + "stop_phrases.txt");
+    	
+    	stopWordsDocument.open();
+    	for (String line = stopWordsDocument.readLine(); line != null; line = stopWordsDocument.readLine()) {
+    		stopWords.add(line.trim());
+    	}
+    	stopWordsDocument.close();
+    	
+    	stopPhrasesDocument.open();
+    	for (String line = stopPhrasesDocument.readLine(); line != null; line = stopPhrasesDocument.readLine()) {
+    		String[] stopPhraseWords = line.split(" ");
+    		LinkedList<String> stopPhrase = new LinkedList<String>();
+    		for (int i = 0; i < stopPhraseWords.length; i++){
+    			stopPhrase.add(stopPhraseWords[i].trim());
+    		}
+    		stopPhrases.add(stopPhrase);
+    	} 	
+    	stopPhrasesDocument.close();
+    	
+    	return new SimpleStopWordsDiscriminator(stopWords, stopPhrases);
+    }
+	
+}
