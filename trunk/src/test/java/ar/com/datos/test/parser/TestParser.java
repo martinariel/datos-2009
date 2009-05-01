409c1b99b4c86900fae4035971c60b6e6b353129
mfernandez <mfernandez@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-01 18:40:39 +0000

    Test del parser un toque mejorado, sigue siendo pesismo

--- trunk/src/test/java/ar/com/datos/test/parser/TestParser.java
+++ trunk/src/test/java/ar/com/datos/test/parser/TestParser.java
@@ -2,24 +2,44 @@ package ar.com.datos.test.parser;
 
 import junit.framework.TestCase;
 import ar.com.datos.parser.Parser;
-import ar.com.datos.documentlibrary.SimpleLinesDocument;
+import ar.com.datos.documentlibrary.MemoryDocument;
 import java.util.Collection;
 
 public class TestParser extends TestCase {
 
 	public void testParser(){
-		SimpleLinesDocument documento = new SimpleLinesDocument();
-		documento.addLine("Prueba linea. Otra Linea ");
-		documento.addLine(" sigue la linea. Fin Linea? Resto");
+		
+		MemoryDocument documento = new MemoryDocument();
+		
+		for (int i = 0; i < 10; i++){
+			documento.addLine("Prueba linea; Otra Linea ");
+			documento.addLine(" sigue la linea. Fin Linea? Resto?");
+		}
 		
 		Parser parser = new Parser(documento);
 		
+		int cantidadPalabras = 0;
+		int cantidadFrases = 0;
+		
 		for (Collection<String> oracion : parser){
-			System.out.println(" _________________");
+			System.out.println("-------");
 			for (String palabra : oracion){
+				cantidadPalabras++;
 				System.out.println(palabra);
 			}
+			cantidadFrases++;
 		}
+	
+		for (Collection<String> oracion : parser){
+			for (String palabra : oracion){
+				cantidadPalabras++;
+			}
+			cantidadFrases++;
+		}
+		
+		assertTrue(cantidadPalabras == 200);	
+		assertTrue(cantidadFrases == 80);
+		
 	}
 	
 }
