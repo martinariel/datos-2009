a1c3ae7ea5b68c9296a6cf7d52c7b85418c79654
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-06-02 05:48:04 +0000

    Simplifico la interfaz de la parte de bits para hacer solo lo que necesita Juan (aritmético). Termino la implementación. Incluyo un test muy pedorro. (Terminado bits, ya puede utilizarse)

--- src/main/java/ar/com/datos/bits/impl/OutputBufferBitReceiver.java
+++ src/main/java/ar/com/datos/bits/impl/OutputBufferBitReceiver.java
@@ -1,7 +1,7 @@
 package ar.com.datos.bits.impl;
 
 import ar.com.datos.bits.BitReceiver;
-import ar.com.datos.bits.BitSequence;
+import ar.com.datos.bits.BitsException;
 import ar.com.datos.buffer.OutputBuffer;
 
 /**
@@ -10,13 +10,11 @@ import ar.com.datos.buffer.OutputBuffer;
  * @author fvalido
  */
 public class OutputBufferBitReceiver implements BitReceiver {
-	/**
-	 * BitSequence usado para completar cada byte.
-	 */
-	private BitSequence bitSequenceBuffer;
-	/**
-	 * OutputBuffer usado como destino de cada byte completado.
-	 */
+	/** Estructura usada para completar cada byte. */
+	private byte[] bitSequenceBuffer;
+	/** Estructura usada para saber la posición actual dentro de bitSequenceBuffer */
+	private byte bitSequencePosition;
+	/** OutputBuffer usado como destino de cada byte completado. */
 	private OutputBuffer outputBuffer;
 	
 	/**
@@ -27,7 +25,8 @@ public class OutputBufferBitReceiver implements BitReceiver {
 	 */
 	public OutputBufferBitReceiver(OutputBuffer outputBuffer) {
 		this.outputBuffer = outputBuffer;
-		this.bitSequenceBuffer = new BitSequenceImpl();
+		this.bitSequenceBuffer = new byte[8];
+		this.bitSequencePosition = 0;
 	}
 	
 	/*
@@ -52,8 +51,16 @@ public class OutputBufferBitReceiver implements BitReceiver {
 	 * Finaliza el agregado de un byte completo.
 	 */
 	private void closeByte() {
-		this.outputBuffer.write(this.bitSequenceBuffer.toDecimalByte(0, (byte)8));
-		this.bitSequenceBuffer.clear();
+		byte bits = 0;
+
+		int pow = 1;
+		for (int i = 7; i >= 0; i--) {
+			bits += this.bitSequenceBuffer[i] * pow;
+			pow *= 2;
+		}
+		
+		this.outputBuffer.write(bits);
+		this.bitSequencePosition = 0;
 	}
 	
 	/*
@@ -61,9 +68,14 @@ public class OutputBufferBitReceiver implements BitReceiver {
 	 * @see ar.com.datos.bits.BitReceiver#addBit(byte)
 	 */
 	@Override
-	public void addBit(byte bit) {
-		this.bitSequenceBuffer.addBit(bit);
-		if (this.bitSequenceBuffer.getBitsCount() == 8) {
+	public void addBit(byte bit) throws BitsException {
+		if (bit != 1 && bit != 0) {
+			throw new BitsException();
+		}
+		
+		this.bitSequenceBuffer[this.bitSequencePosition] = bit;
+		this.bitSequencePosition++;
+		if (this.bitSequencePosition == 8) {
 			closeByte();
 		}
 	}
@@ -74,6 +86,13 @@ public class OutputBufferBitReceiver implements BitReceiver {
 	 */
 	@Override
 	public void close() {
-		closeByte();
+		if (this.bitSequencePosition != 0) {
+			// Pongo en 0 los bits de la derecha del bitSequenceBuffer que no fueron establecidos.
+			for (int i = this.bitSequencePosition; i < 8; i++) {
+				this.bitSequenceBuffer[i] = 0;
+			}
+			// Cierro el byte normalmente.
+			closeByte();
+		}
 	}
 }
