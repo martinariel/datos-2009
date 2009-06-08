a62f20e10992fc0e5f2172c974af728250d69bdd
fvalido <fvalido@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-06-08 02:39:04 +0000

    ESC y EOF (lo incluí pero en realidad debería ser usado siempre por rango en nuestro caso!) en SuperChar. ProbabilityTable con soporte para ESC. Correción de un par de bugs.

--- trunk/src/test/java/ar/com/datos/test/compressor/TestProbabilityTableByFrequencies.java
+++ trunk/src/test/java/ar/com/datos/test/compressor/TestProbabilityTableByFrequencies.java
@@ -15,6 +15,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		SuperChar highestSuperChar = new SimpleSuperChar(10);
 		ProbabilityTableByFrequencies probabilityTable = new ProbabilityTableByFrequencies(lowestSuperChar, highestSuperChar);
 		
+		assertEquals(11, probabilityTable.getNumberOfChars());
 		assertEquals(11, probabilityTable.countCharsWithProbabilityUnder((double)1/5));		
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/11));
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/100));
@@ -29,6 +30,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		}
 		
 		probabilityTable.addOcurrency(new SimpleSuperChar(5));
+		assertEquals(11, probabilityTable.getNumberOfChars());
 		assertEquals(11, probabilityTable.countCharsWithProbabilityUnder((double)1/5));
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/12));
 		assertEquals(10, probabilityTable.countCharsWithProbabilityUnder((double)2/12));
@@ -36,7 +38,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		currentTuple = it.next();
 		assertEquals(new SimpleSuperChar(5), currentTuple.getFirst());
 		assertEquals((double)2/12, currentTuple.getSecond());
-		i = 0;
+		i = lowestSuperChar.intValue();;
 		while (it.hasNext()) {
 			currentTuple = it.next();
 			assertEquals(new SimpleSuperChar(i), currentTuple.getFirst());
@@ -48,6 +50,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		}
 		
 		probabilityTable.addOcurrency(new SimpleSuperChar(20));
+		assertEquals(12, probabilityTable.getNumberOfChars());
 		assertEquals(12, probabilityTable.countCharsWithProbabilityUnder((double)1/5));
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/13));
 		assertEquals(11, probabilityTable.countCharsWithProbabilityUnder((double)2/13));
@@ -58,7 +61,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		currentTuple = it.next();
 		assertEquals(new SimpleSuperChar(20), currentTuple.getFirst());
 		assertEquals((double)1/13, currentTuple.getSecond());
-		i = 0;
+		i = lowestSuperChar.intValue();;
 		while (it.hasNext()) {
 			currentTuple = it.next();
 			assertEquals(new SimpleSuperChar(i), currentTuple.getFirst());
@@ -76,6 +79,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		ProbabilityTableByFrequencies probabilityTable = new ProbabilityTableByFrequencies(lowestSuperChar, highestSuperChar);
 		
 		probabilityTable.addToExcludedSet(new SimpleSuperChar(5));
+		assertEquals(10, probabilityTable.getNumberOfChars());
 		assertEquals(10, probabilityTable.countCharsWithProbabilityUnder((double)1/5));		
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/10));
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/100));
@@ -93,6 +97,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		}
 
 		probabilityTable.addToExcludedSet(new SimpleSuperChar(20));
+		assertEquals(10, probabilityTable.getNumberOfChars());
 		assertEquals(10, probabilityTable.countCharsWithProbabilityUnder((double)1/5));		
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/10));
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/100));
@@ -110,6 +115,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		
 		probabilityTable.addOcurrency(new SimpleSuperChar(5));
 		probabilityTable.addOcurrency(new SimpleSuperChar(6));
+		assertEquals(10, probabilityTable.getNumberOfChars());
 		assertEquals(10, probabilityTable.countCharsWithProbabilityUnder((double)1/5));
 		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/11));
 		assertEquals(9, probabilityTable.countCharsWithProbabilityUnder((double)2/11));
@@ -117,7 +123,7 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		currentTuple = it.next();
 		assertEquals(new SimpleSuperChar(6), currentTuple.getFirst());
 		assertEquals((double)2/11, currentTuple.getSecond());
-		i = 0;
+		i = lowestSuperChar.intValue();;
 		while (it.hasNext()) {
 			currentTuple = it.next();
 			assertEquals(new SimpleSuperChar(i), currentTuple.getFirst());
@@ -129,4 +135,46 @@ public class TestProbabilityTableByFrequencies extends TestCase {
 		}
 	}
 
+	public void testESC() {
+		SuperChar lowestSuperChar = new SimpleSuperChar(0);
+		SuperChar highestSuperChar = new SimpleSuperChar(10);
+		ProbabilityTableByFrequencies probabilityTable = new ProbabilityTableByFrequencies(lowestSuperChar, highestSuperChar);
+		
+		probabilityTable.addOcurrency(SuperChar.ESC);
+		probabilityTable.addOcurrency(SuperChar.ESC);
+		probabilityTable.addOcurrency(SuperChar.ESC);
+		probabilityTable.addOcurrency(SuperChar.EOF);
+		probabilityTable.addOcurrency(new SimpleSuperChar(5));
+		probabilityTable.addOcurrency(new SimpleSuperChar(5));
+		probabilityTable.addOcurrency(new SimpleSuperChar(5));
+		assertEquals(13, probabilityTable.getNumberOfChars());
+		assertEquals(13, probabilityTable.countCharsWithProbabilityUnder((double)1/2));		
+		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/18));
+		assertEquals(11, probabilityTable.countCharsWithProbabilityUnder((double)2/18));
+		assertEquals(11, probabilityTable.countCharsWithProbabilityUnder((double)3/18));
+		assertEquals(12, probabilityTable.countCharsWithProbabilityUnder((double)4/18));
+		assertEquals(13, probabilityTable.countCharsWithProbabilityUnder((double)5/18));
+		assertEquals(0, probabilityTable.countCharsWithProbabilityUnder((double)1/100));
+		Iterator<Tuple<SuperChar, Double>> it = probabilityTable.iterator();
+		Tuple<SuperChar, Double> currentTuple;
+		currentTuple = it.next();
+		assertEquals(new SimpleSuperChar(5), currentTuple.getFirst());
+		assertEquals((double)4/18, currentTuple.getSecond());
+		currentTuple = it.next();
+		assertEquals(SuperChar.EOF, currentTuple.getFirst());
+		assertEquals((double)1/18, currentTuple.getSecond());
+		int i = lowestSuperChar.intValue();
+		while (i <= highestSuperChar.intValue()) {
+			currentTuple = it.next();
+			assertEquals(new SimpleSuperChar(i), currentTuple.getFirst());
+			assertEquals((double)1/18, currentTuple.getSecond());
+			i++;
+			if (i == 5) {
+				i++;
+			}
+		}
+		currentTuple = it.next();
+		assertEquals(SuperChar.ESC, currentTuple.getFirst());
+		assertEquals((double)3/18, currentTuple.getSecond());
+	}
 }
